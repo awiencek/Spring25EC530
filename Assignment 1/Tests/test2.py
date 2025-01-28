f2bdbfead39bd67638bf550c9f5d80609a731d12
awiencek <114934605+awiencek@users.noreply.github.com>
2025-01-28 18:47:02 -0500

    Create test2.py

---
+++ Assignment 1/Tests/test2.py
@@ -0,0 +1,5 @@
+# Find the closest airport to a city
+# First Input:  list of major world cities:  https://raw.githubusercontent.com/joelacus/world-cities/refs/heads/main/world_cities.csv 
+# Second Vector:  List of major world airports:  https://github.com/ip2location/ip2location-iata-icao/blob/master/iata-icao.csv
+# Future Bonus:  Make your output and input configurables…. Pass a CSV or JSON and get the output as a JSON for the user.
+# Why is that?
