28c97d4a5114cd6cb847d66db34f4b0e83e49c05
awiencek <114934605+awiencek@users.noreply.github.com>
2025-01-28 18:51:56 -0500

    Create test5.py

---
+++ Assignment 1/Tests/test5.py
@@ -0,0 +1,9 @@
+# additional input problems/errors users can encounter
+
+#additional things that can be tested
+#csv with missing data cells
+#mixed input units
+#output units
+#extra characters (direction, units, etc) in input data
+#non number input
+#improper array formatting
