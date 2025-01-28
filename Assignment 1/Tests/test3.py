63c2e27b29d824b35c9d865764d0e11e296b3378
awiencek <114934605+awiencek@users.noreply.github.com>
2025-01-28 18:47:50 -0500

    Create test3.py

---
+++ Assignment 1/Tests/test3.py
@@ -0,0 +1,4 @@
+# First input is:
+# https://docs.google.com/spreadsheets/d/1oJy_VQ08Fjdj-hwMXYTR0dUTlrahBTyPRlJQrjuK9ZE/edit?usp=sharing 
+# Second Vector:  List of major world airports: 
+# https://github.com/ip2location/ip2location-iata-icao/blob/master/iata-icao.csv
