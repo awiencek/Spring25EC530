8531a0a1df2c950aaa21b4d5f83508c66b8d9bf8
awiencek <114934605+awiencek@users.noreply.github.com>
2025-01-28 18:49:09 -0500

    Create test4.py

---
+++ Assignment 1/Tests/test4.py
@@ -0,0 +1,8 @@
+# Study the following files...
+# https://drive.google.com/file/d/1B5OVEcLH0iM23DDqc2PlGGPhTl5xmw77/view?usp=sharing
+# https://drive.google.com/file/d/1Iw3MJxakfyIUzK21GpEoOtHBFmYuqrX6/view?usp=sharing 
+
+# What did you learn?
+
+# First input is: one of the files
+# Second Vector:  List of major world airports:  https://github.com/ip2location/ip2location-iata-icao/blob/master/iata-icao.csv
