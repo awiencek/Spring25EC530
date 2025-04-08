e5ef713bc245c9547451fce85fc0a37f8e04fef0
awiencek <114934605+awiencek@users.noreply.github.com>
2025-04-08 18:38:43 -0400

    Update __init__.py

--- Assignment 1/geo_distance/__init__.py
+++ Assignment 1/geo_distance/__init__.py
@@ -1,1 +1,10 @@
+# geo_distance/__init__.py
+
+"""
+geo_distance - A Python package to calculate geographic distances and find the closest locations.
+"""
+
+from .geo_calculations import gps_distance, decide_min_geodistance
+
+__all__ = ['gps_distance', 'decide_min_geodistance']
 
